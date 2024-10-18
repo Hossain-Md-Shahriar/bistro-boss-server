39bd9832a1eaec53e52344bb02e6d051c8e0cdf9
Hossain-Md-Shahriar <hasibshahriar1@gmail.com>
2024-10-18 21:56:57 +0600

    cart collection API for get data using email & post a data

--- index.js
+++ index.js
@@ -24,10 +24,30 @@ async function run() {
   try {
     const menuCollection = client.db("BistroDB").collection("menu");
     const reviewsCollection = client.db("BistroDB").collection("reviews");
+    const cartCollection = client.db("BistroDB").collection("carts");
 
     app.get("/menu", async (req, res) => {
-        const result = await menuCollection.find().toArray();
-        res.send(result);
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // carts collection
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
     });
   } finally {
   }
@@ -41,3 +61,15 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Bistro Boss is sitting on port ${port}`);
 });
+
+/**
+ * ------------------------
+ *    Naming Convention
+ * ------------------------
+ * app.get("/users")
+ * app.get("/users/:id")
+ * app.post("/users")
+ * app.put("/users/:id")
+ * app.patch("/users/:id")
+ * app.delete("/users/:id")
+ */
