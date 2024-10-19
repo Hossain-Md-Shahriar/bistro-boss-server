c271429c609dade25be9b22d78ccc46f3a4daa70
Hossain-Md-Shahriar <hasibshahriar1@gmail.com>
2024-10-19 21:42:47 +0600

    user collection API to make a user admin and also delete user

--- index.js
+++ index.js
@@ -46,6 +46,25 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // menu related APIs
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
