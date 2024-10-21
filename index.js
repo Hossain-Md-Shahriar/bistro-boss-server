ee46f16c94213f5279999d8b70a389322dbe2f48
Hossain-Md-Shahriar <hasibshahriar1@gmail.com>
2024-10-21 23:06:30 +0600

    menuCollection API created to add new menu Items

--- index.js
+++ index.js
@@ -129,6 +129,12 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
+      const menuItem = req.body;
+      const result = await menuCollection.insertOne(menuItem);
+      res.send(result);
+    });
+
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
